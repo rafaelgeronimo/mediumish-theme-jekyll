cd36adfe6ae53feeadeed824faee36c4e9b7b349
rafaelgeronimo <rafaelgeronimo@outlook.com>
2019-01-28 10:09:58 -0200

    Year adjust

--- home/assets/js/footer.js
+++ home/assets/js/footer.js
@@ -1,32 +1,26 @@
 // Obtain the footer
-const rootElement = document.getElementById('footer')
-// Create a ES6 class component    
-    class Footer extends React.Component { 
-// Use the render function to return JSX component      
-    render() { 
-        return (
-            <footer className="footer">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6 col-sm-6 text-center text-lg-left">
-                            Copyright © 2018 - {this.props.name}
-                        </div>
-                    </div>
-                </div>
-            </footer>
-      );
-      } 
-    }
+const rootElement = document.getElementById("footer");
+// Create a ES6 class component
+class Footer extends React.Component {
+  // Use the render function to return JSX component
+  render() {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-sm-6 text-center text-lg-left">
+              Copyright © 2019 - {this.props.name}
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
 // Create a function to wrap up your component
-function App(){
-  return(
-    <Footer name="Rafael Gerônimo" />
-  )
+function App() {
+  return <Footer name="Rafael Gerônimo" />;
 }
 
-
 // Use the ReactDOM.render to show your component on the browser
-    ReactDOM.render(
-      <App />,
-      rootElement
-    )
+ReactDOM.render(<App />, rootElement);
